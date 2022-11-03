12f90549043e04868af869a086cdd502cc8ae5e0
Vardhan Goud <vardhangoud41@gmail.com>
2022-11-03 21:15:54 +0530

    Weather Forecast Application

--- weather-app/src/Components/Header.js
+++ weather-app/src/Components/Header.js
@@ -3,64 +3,71 @@ import logo from '../logo.gif'
 import APi from './APi'
 // import background from '../background.jpeg'
 import Home from './Home'
+import Temp from './Temp'
 function Header() {
-    const [popup, setPopup] = useState(false)
-    const [model, setModel] = useState(false)
-    function weatherSubmit(e) {
-        e.preventDefault()
-        setPopup(false)
-        setModel(false)
-    }
-    function homeSubmit(e) {
-        e.preventDefault()
-        setPopup(true)
-        setModel(false)
-    }
-    function GetSubmit(e) {
-      e.preventDefault()
-      setModel(true)
-      setPopup(false)
-    }
+  const [popup, setPopup] = useState(false);
+  const [model, setModel] = useState(false);
+  const [temperature, setTemperature] = useState(false);
+  function weatherSubmit(e) {
+    e.preventDefault();
+    setPopup(false);
+    setModel(false);
+    setTemperature(false);
+  };
+  function homeSubmit(e) {
+    e.preventDefault();
+    setPopup(true);
+    setModel(false);
+    setTemperature(false);
+  };
+  function GetSubmit(e) {
+    e.preventDefault();
+    setModel(true);
+    setPopup(false);
+    setTemperature(false);
+  };
 
-    const TemperatureSumit = (e) => {
-        e.preventDefault()
-        setPopup(false)
-        setModel(false)
-    }
+  const TemperatureSumit = (e) => {
+    e.preventDefault();
+    setPopup(false);
+    setModel(false);
+    setTemperature(true);
+  };
 
-    const HomepageSubmit = (e) => {
-        e.preventDefault()
-        setPopup(false)
-        setModel(false)
-    }
+  const HomepageSubmit = (e) => {
+    e.preventDefault();
+    setPopup(false);
+    setModel(false);
+    setTemperature(false);
+  };
   return (
     <>
       <div className='header'>
         <nav>
-            <ul className='nav-buttons'>
-            <img onClick={HomepageSubmit} className='cloud-img' src= {logo} alt="cloud item" />
-                <li className='list-dot' ><a onClick={weatherSubmit} id='list-item1' href="#">Weather Forecast App</a></li>
-                <li className='list-dot' ><a onClick={homeSubmit} id='list-item2' href="#">Home</a></li>
-                
-                <li className='list-dot' ><a onClick={GetSubmit} id='list-item3' href="#">Get Weather</a></li>
-                <li className='list-dot' ><a onClick={TemperatureSumit} id='list-item4' href="#">Temperature Converter</a></li>
-                
-            </ul>
-        </nav>
-        
-      </div>
-      
-        <div className='body'>
-          <div className='child-body'>
-          <Home show = {popup}></Home>
-          <APi visible = {model}></APi>
-          </div>
-        {/* <img className='image-file' src= {background} alt=""  /> */}
-        
-       
+          <ul className='nav-buttons'>
+            <img onClick={HomepageSubmit} className='cloud-img' src={logo} alt="cloud item" />
+            <li className='list-dot' ><a onClick={weatherSubmit} id='list-item1' href="./Header.js">Weather Forecast App</a></li>
+            <li className='list-dot' ><a onClick={homeSubmit} id='list-item2' href="./Home">Home</a></li>
+            <li className='list-dot' ><a onClick={GetSubmit} id='list-item3' href='./Api'>Get Weather</a></li>
+            <li className='list-dot' ><a onClick={TemperatureSumit} id='list-item4' href="./Temp">Temperature Converter</a></li>
 
+          </ul>
+        </nav>
+
+      </div>
+
+      <div className='body'>
+        <div className='child-body'>
+          <Home show={popup}></Home>
+          <APi visible={model}></APi>
+          <Temp showpop={temperature} />
         </div>
-      
+        {/* <img className='image-file' src= {background} alt=""  /> */}
+
+
+
+      </div>
+
     </>
   )
 }
--- weather-app/src/Components/Home.js
+++ weather-app/src/Components/Home.js
@@ -1,31 +1,30 @@
 import React from 'react'
 
 function Home(props) {
-    if(!props.show) {
-        return null
-    }
+  if (!props.show) {
+    return null;
+  }
   return (
     <div>
       <div className='box'>
         <div>
-        <h2>Welcome, to our Forecast Weather App</h2>
+          <h2>Welcome, to our Forecast Weather App</h2>
         </div>
-       <div>
-       <p>Weather forecasting is the application of science and technology to predict 
-        the conditions of the atmosphere for a given location and time. People have 
-        attempted to predict the weather informally for millennia and formally since 
-        the 19th century.Once calculated manually based mainly upon changes in barometric 
-        pressure, current weather conditions, and sky condition or cloud cover, weather 
-        forecasting now relies on computer-based models that take many atmospheric factors 
-        into account.There is a vast variety of end uses to weather forecasts. Weather warnings
-         are important forecasts because they are used to protect life and property.Forecasts 
-         based on temperature and precipitation are important to agriculture, and therefore 
-         to traders within commodity markets.</p>
-       </div>
-      
+        <div>
+          <p>Weather forecasting is the application of science and technology to predict
+            the conditions of the atmosphere for a given location and time. 
+            </p>
+            <p id='para-2'>People haveattempted to predict the weather informally for millennia and formally since
+            the 19th century.</p>
+            <p className='para-3' >Weather forecasts are made by collecting quantitative data about the current state of the atmosphere,
+             land, and ocean and using meteorology to project how the atmosphere will change at a given place.</p>
+             <p className='para-4'> Hence, forecasts become less accurate as the difference between current time and the time for which the forecast 
+              is being made (the range of the forecast) increases.</p>
+        </div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
